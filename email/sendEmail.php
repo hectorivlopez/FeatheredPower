c9ca19aa007f86cf9b56ab9f6b128e32b3e3f208
hectorivlopez <hectorivlopez@gmail.com>
2023-10-17 17:51:04 -0600

    Final adjustments

--- email/sendEmail.php
+++ email/sendEmail.php
@@ -67,8 +67,6 @@ if (isset($_POST)) {
 	$pdfdoc = $pdf->Output('S', '');
 
 	/* --------------- Save pdf in WebDAV --------------- */
-
-	// ----- Using FTP as an intermediary -----
 	// Generate unique pdf name
 	date_default_timezone_set('America/Mexico_City');
 	$pdfFolder = __DIR__ . '/pdfs/';
@@ -77,34 +75,76 @@ if (isset($_POST)) {
 	// Save pdf in folder
 	$pdf->Output('F', $pdfFolder . $pdfName);
 
+	// ----- Saving directly in WebDAV -----
+	// Parameters
+	$localFilePath = $pdfFolder . $pdfName;
+	$webdavUrl = "http://10.0.0.5/pdfs/$pdfName";
+	$username = 'hector';
+	$password = '12';
+
+	// Initialize curl session
+	$ch = curl_init();
+
+	// Curl options
+	curl_setopt($ch, CURLOPT_URL, $webdavUrl);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+	curl_setopt($ch, CURLOPT_UPLOAD, 1);
+	curl_setopt($ch, CURLOPT_INFILE, fopen($localFilePath, 'rb'));
+	curl_setopt($ch, CURLOPT_INFILESIZE, filesize($localFilePath));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+
+	// Execute curl request
+	$result = curl_exec($ch);
+
+	// Check for errors
+	if ($result === false) {
+		echo 'Error: ' . curl_error($ch);
+	} else {
+		echo 'PDF uploaded  successfully.';
+	}
+
+	// Close curl session
+	curl_close($ch);
+
+	unlink($pdfFolder . $pdfName);
+
+	// Increment user orders 
+	$orders = $user['orders'] + 1;
+	$query = "UPDATE users SET orders = '$orders' WHERE id = '$userId'";
+	$result = mysqli_query($db, $query);
+
+	header('location: /cart.php?result=1');
+
+
+	// ----- Using FTP as an intermediary -----
 	/* // Parameters
-	$httpFileURL = '' . $pdfName; 
-	$ftpServer = '';
+	$localFilePath = $pdfFolder . $pdfName; 
+	$ftpServer = 'http://10.0.0.6';
 	$ftpUser = 'user1';
 	$ftpPassword = '12';
-	$webdavServerURL = '';
+	$webdavUrl = 'http://10.0.0.5';
 	$webdavUser = 'hector';
 	$webdavPassword = '12';
 	$destinationFilename = $pdfName; 
- */
-	/* // Method 1
-	// Initialize cURL
+
+	// Initialize curl
 	$ch = curl_init();
 
-	// Set cURL options
-	curl_setopt($ch, CURLOPT_URL, $httpFileURL);
+	// Curl options
+	curl_setopt($ch, CURLOPT_URL, $localFilePath);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_USERPWD, "$ftpUser:$ftpPassword");
 	curl_setopt($ch, CURLOPT_UPLOAD, 1);
-	curl_setopt($ch, CURLOPT_INFILE, fopen($httpFileURL, 'rb'));
-	curl_setopt($ch, CURLOPT_INFILESIZE, filesize($httpFileURL));
+	curl_setopt($ch, CURLOPT_INFILE, fopen($localFilePath, 'rb'));
+	curl_setopt($ch, CURLOPT_INFILESIZE, filesize($localFilePath));
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
 	curl_setopt($ch, CURLOPT_URL, $ftpServer . $destinationFilename);
 
-	// Execute cURL request
+	// Execute curl request
 	$result = curl_exec($ch);
 
-	// Close cURL session
+	// Close curl session
 	curl_close($ch);
 
 	if ($result === false) {
@@ -114,7 +154,7 @@ if (isset($_POST)) {
 
 		// Notify the WebDAV server to move the file
 		$ch2 = curl_init();
-		$webdavMoveURL = $webdavServerURL . $destinationFilename;
+		$webdavMoveURL = $webdavUrl . $destinationFilename;
 		curl_setopt($ch2, CURLOPT_URL, $webdavMoveURL);
 		curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, 'MOVE');
 		curl_setopt($ch2, CURLOPT_POSTFIELDS, '');
@@ -133,79 +173,6 @@ if (isset($_POST)) {
 			echo 'File moved to WebDAV server successfully.';
 		}
 	} */
-
-	/* // Method 2
-	// Establish an FTP connection
-	$ftpConnection = ftp_connect($ftpServer);
-
-	// Log in to the FTP server
-	if (ftp_login($ftpConnection, $ftpUser, $ftpPassword)) {
-		$localFile = $pdfFolder . $pdfName;
-
-		// Upload the file to the FTP server
-		if (ftp_put($ftpConnection, $destinationFilename, $localFile, FTP_BINARY)) {
-			// Notify the WebDAV server to move the file
-			$webdavMoveURL = $webdavServerURL . $destinationFilename;
-			file_get_contents($webdavMoveURL, false, stream_context_create([
-				'http' => [
-					'method' => 'MOVE',
-					'header' => 'Destination: ' . $webdavMoveURL,
-				]
-			]));
-
-			echo 'File transferred and moved successfully.';
-		} else {
-			echo 'FTP file upload failed.';
-		}
-
-		// Close the FTP connection
-		ftp_close($ftpConnection);
-	} else {
-		echo 'FTP login failed.';
-	}
-
-	exit; */
-
-	// ----- Saving directly in WebDAV -----
-	// Initialize cURL session
-	$localFilePath = $pdfFolder . $pdfName; 
-	$webdavServerURL = "http://10.0.0.5/pdfs/$pdfName"; 
-	$username = 'hector';
-	$password = '12';
-
-	// Initialize cURL session
-	$ch = curl_init();
-
-	// Set cURL options
-	curl_setopt($ch, CURLOPT_URL, $webdavServerURL);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-	curl_setopt($ch, CURLOPT_UPLOAD, 1);
-	curl_setopt($ch, CURLOPT_INFILE, fopen($localFilePath, 'rb'));
-	curl_setopt($ch, CURLOPT_INFILESIZE, filesize($localFilePath));
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-
-	// Execute cURL request
-	$result = curl_exec($ch);
-
-	// Check for errors
-	if ($result === false) {
-		echo 'cURL error: ' . curl_error($ch);
-	} else {
-		echo 'File uploaded or overwritten successfully.';
-	}
-
-	// Close cURL session
-	curl_close($ch);
-	
-	unlink($pdfFolder . $pdfName);
-
-	// Increment user orders 
-	$orders = $user['orders'] + 1;
-	$query = "UPDATE users SET orders = '$orders' WHERE id = '$userId'";
-	$result = mysqli_query($db, $query);
-
-	header('location: /cart.php?result=1');
 
 
 	/* --------------- Send email using PHPMailer --------------- */
